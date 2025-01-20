42b13da75d82d6774ec5a0f2672f10db51704a3f
victorfriasv <victorfriasv@users.noreply.github.com>
2025-01-20 12:22:30 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -184,27 +184,28 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Boda Hawaiana - Antonio y Víctor",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Sep 13, 2025 13:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            //end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'La Casita de Pinto',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: ""
         }
     });
 
     $('#add-to-cal').html(myCalendar);
+
 
 
     /********************** RSVP **********************/
@@ -218,7 +219,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwRyvYxGADHHI6M9yzJuR-jZg-zH1dy0-2sNK5pGkkf-QSN2WkD61F_F-6JTBuFwXY9zQ/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {

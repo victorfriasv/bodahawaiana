11362b8bfc8f038072a631df58bb10481b353d83
victorfriasv <victorfriasv@users.noreply.github.com>
2025-01-20 12:51:58 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -184,57 +184,60 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Boda Hawaiana - Antonio y Víctor",
+            title: "Ram and Antara's Wedding",
 
             // Event start date
-            start: new Date('Sep 13, 2025 13:00'),
+            start: new Date('Nov 27, 2017 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            //end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'La Casita de Pinto',
+            address: 'ITC Fortune Park Hotel, Kolkata',
 
             // Event Description
-            description: ""
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
         }
     });
 
     $('#add-to-cal').html(myCalendar);
 
 
-
     /********************** RSVP **********************/
-    $('#rsvp-form').on('submit', function (e) {
-        e.preventDefault();
-        var data = $(this).serialize();
+/********************** RSVP **********************/
+$('#rsvp-form').on('submit', function (e) {
+    e.preventDefault();
+    // Recoge todos los datos del formulario
+    var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+    $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbwRyvYxGADHHI6M9yzJuR-jZg-zH1dy0-2sNK5pGkkf-QSN2WkD61F_F-6JTBuFwXY9zQ/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    if (data.result === "error") {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
-                    } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
-                    }
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                });
-        }
-    });
+    // Validación del código de invitación (SIN CAMBIOS)
+    if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+    } else {
+        // Envía los datos, incluyendo los nuevos campos
+        $.post('https://script.google.com/macros/s/AKfycbwRyvYxGADHHI6M9yzJuR-jZg-zH1dy0-2sNK5pGkkf-QSN2WkD61F_F-6JTBuFwXY9zQ/exec', data)
+            .done(function (data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html('');
+                    $('#rsvp-modal').modal('show');
+                }
+            })
+            .fail(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
+    }
+});
 
 });
 

7c025bebef3b27f002997d79fd5eedca5ba8014c
victorfriasv <victorfriasv@users.noreply.github.com>
2025-01-24 08:11:58 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -215,7 +215,7 @@ $('#rsvp-form').on('submit', function (e) {
     $('#alert-wrapper').html(alert_markup('info', '<strong>Espera un momento.</strong> Estamos guardando tus respuestas.'));
 
         // Envía los datos, incluyendo los nuevos campos
-        $.post('https://script.google.com/macros/s/AKfycbzhe-5GkzNKPC-XqxLqqvEgclcQfuu1_a_EBeduVM7gw6A9MZZeq_9fh7UmWuYDwO4cnA/exec', data)
+        $.post('https://script.google.com/macros/s/AKfycbwMN41KYmD_dlPvF1vO9-JqfHWAac27rh5mTsRSLCMl7AzF0Ycuie7qAb5WTeCvO-kEnQ/exec', data)
             .done(function (data) {
                 console.log(data);
                 if (data.result === "error") {
